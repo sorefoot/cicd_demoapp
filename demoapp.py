b272e4df87010c18dc5dd9009d06988901683577
Clint Sorensen <clsorens@cisco.com>
2017-03-30 12:09:38 -0600

    added Sad and Cruel

--- demoapp.py
+++ demoapp.py
@@ -8,14 +8,14 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        text = "Hello World!"
+        text = "Hello Sad World!"
         return text
 
 api.add_resource(HelloWorld, '/hello/world')
 
 class HelloUniverse(Resource):
     def get(self):
-        text = "Hello Universe!"
+        text = "Hello Cruel Universe!"
         return text
 
 api.add_resource(HelloUniverse, '/hello/universe')
